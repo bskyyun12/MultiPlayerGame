b45f2f6be0a2cd00fd00947844a5f54030f57187
= <=>
2020-11-13 06:21:25 +0100

    CDN test

--- Source/MultiplayGame/MultiplayGameGameModeBase.cpp
+++ Source/MultiplayGame/MultiplayGameGameModeBase.cpp
@@ -1,10 +1,10 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-
 #include "MultiplayGameGameModeBase.h"
 
 AMultiplayGameGameModeBase::AMultiplayGameGameModeBase()
-{
+{	
+	// 
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/PerceptiveAI/Characters/Player/BP_Player"));
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
